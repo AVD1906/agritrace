e6228685e3530a6a1086ab5b2b7ecf2ff1c0cb56
Amisha Vidya Diwakar <amishavidyadiwakar@gmail.com>
2026-07-05 22:54:44 +0530

    cleanup

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const notificationController = require('../controllers/notificationController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// 🔔 GET MY NOTIFICATIONS
+
 router.get('/', authMiddleware, notificationController.getNotifications);
 
-// ➕ CREATE NOTIFICATION
+
 router.post('/', authMiddleware, notificationController.createNotification);
 
-// ✅ MARK AS READ
+
 router.put('/read/:id', authMiddleware, notificationController.markAsRead);
 
 module.exports = router;
